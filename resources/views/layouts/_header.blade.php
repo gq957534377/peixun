d0596d2c7882ce4597f31598cf8bcfbcd6edbc07
jing-yue <xyyspace@163.com>
2019-01-02 21:25:46 +0800

    修改页面头部信息

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,7 +4,7 @@
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>A</b>LT</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg">{{ config('app.name', '危险化学品安全监管平台') }}</span>
+        <span class="logo-lg">{{ config('app.name', '数据管理平台') }}</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
