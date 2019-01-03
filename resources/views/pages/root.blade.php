5b56904a4be47474f553432e50b4c0870a41474a
jing-yue <xyyspace@163.com>
2019-01-03 16:24:53 +0800

    更改首页

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -29,57 +29,57 @@
             </div>
           </div>
           <!-- /.box-header -->
-          <div class="box-body table-responsive no-padding">
-            <table class="table table-hover">
-              <tr>
-                <th>ID</th>
-                <th>User</th>
-                <th>Date</th>
-                <th>Status</th>
-                <th>Reason</th>
-              </tr>
-              <tr>
-                <td>183</td>
-                <td>John Doe</td>
-                <td>11-7-2014</td>
-                <td><span class="label label-success">Approved</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
-              <tr>
-                <td>219</td>
-                <td>Alexander Pierce</td>
-                <td>11-7-2014</td>
-                <td><span class="label label-warning">Pending</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
-              <tr>
-                <td>657</td>
-                <td>Bob Doe</td>
-                <td>11-7-2014</td>
-                <td><span class="label label-primary">Approved</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
-              <tr>
-                <td>175</td>
-                <td>Mike Doe</td>
-                <td>11-7-2014</td>
-                <td><span class="label label-danger">Denied</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
-            </table>
-            <div class="row">
-              <div class="col-sm-7">
-                <div class="float-left">
+          {{--<div class="box-body table-responsive no-padding">--}}
+            {{--<table class="table table-hover">--}}
+              {{--<tr>--}}
+                {{--<th>ID</th>--}}
+                {{--<th>User</th>--}}
+                {{--<th>Date</th>--}}
+                {{--<th>Status</th>--}}
+                {{--<th>Reason</th>--}}
+              {{--</tr>--}}
+              {{--<tr>--}}
+                {{--<td>183</td>--}}
+                {{--<td>John Doe</td>--}}
+                {{--<td>11-7-2014</td>--}}
+                {{--<td><span class="label label-success">Approved</span></td>--}}
+                {{--<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>--}}
+              {{--</tr>--}}
+              {{--<tr>--}}
+                {{--<td>219</td>--}}
+                {{--<td>Alexander Pierce</td>--}}
+                {{--<td>11-7-2014</td>--}}
+                {{--<td><span class="label label-warning">Pending</span></td>--}}
+                {{--<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>--}}
+              {{--</tr>--}}
+              {{--<tr>--}}
+                {{--<td>657</td>--}}
+                {{--<td>Bob Doe</td>--}}
+                {{--<td>11-7-2014</td>--}}
+                {{--<td><span class="label label-primary">Approved</span></td>--}}
+                {{--<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>--}}
+              {{--</tr>--}}
+              {{--<tr>--}}
+                {{--<td>175</td>--}}
+                {{--<td>Mike Doe</td>--}}
+                {{--<td>11-7-2014</td>--}}
+                {{--<td><span class="label label-danger">Denied</span></td>--}}
+                {{--<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>--}}
+              {{--</tr>--}}
+            {{--</table>--}}
+            {{--<div class="row">--}}
+              {{--<div class="col-sm-7">--}}
+                {{--<div class="float-left">--}}
                   {{--{!! $drugs->total() !!} 共计--}}
-                </div>
-              </div><!--col-->
-              <div class="col-5">
-                <div class="float-right">
+                {{--</div>--}}
+              {{--</div><!--col-->--}}
+              {{--<div class="col-5">--}}
+                {{--<div class="float-right">--}}
                   {{--{!! $drugs->render() !!}--}}
-                </div>
-              </div><!--col-->
-            </div><!--row-->
-          </div>
+                {{--</div>--}}
+              {{--</div><!--col-->--}}
+            {{--</div><!--row-->--}}
+          {{--</div>--}}
           <!-- /.box-body -->
         </div>
         <!-- /.box -->
