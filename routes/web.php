d22a051293abdd7fd2f4aed75cb4b2cd89073ec9
jing-yue <xyyspace@163.com>
2019-01-03 16:22:01 +0800

    更改路由

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
-    Route::get('/', 'PagesController@root')->name('root');
+    Route::get('/', 'TrainController@index')->name('index');
     Route::post('/uploadImg', 'PagesController@uploadImg');
     Route::group(['namespace' => 'Web'], function () {
         Route::resource('/users', 'UserController');
